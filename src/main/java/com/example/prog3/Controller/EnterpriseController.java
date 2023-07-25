06a90fbeef4ba3fd306bb92d2b87f19d64101287
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-07-25 16:40:48 +0300

    fix: database wrong convert

---
+++ src/main/java/com/example/prog3/Controller/EnterpriseController.java
@@ -0,0 +1,46 @@
+package com.example.prog3.Controller;
+
+import com.example.prog3.Service.EnterpriseService;
+import com.example.prog3.Service.PhoneService;
+import com.example.prog3.model.Employee;
+import com.example.prog3.model.Enterprise;
+import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+@Controller
+@AllArgsConstructor
+public class EnterpriseController extends TokenController{
+    private EnterpriseService enterpriseService;
+    private PhoneService phoneService;
+
+    @GetMapping("/enterprise")
+    public String formEnterprise(@ModelAttribute Enterprise enterprise, Model model){
+        model.addAttribute("enterprise",enterpriseService.getEnterprise());
+        return "updateEnterprise";
+    }
+
+    @PostMapping("/updateEnt")
+    public String upEmployee(
+            @RequestParam("Img") MultipartFile file,
+            @RequestParam("name") String name ,
+            @RequestParam("desc") String desc,
+            @RequestParam("slogan") String slogan,
+            @RequestParam("address") String address,
+            @RequestParam("email") String email,
+            @RequestParam("nif") String nif,
+            @RequestParam("stat") String stat,
+            @RequestParam("rcs") String rcs,
+            @RequestParam("phoneNumbers") String phoneNumbers) throws IOException {
+        byte[] fileData = file.getBytes();
+        enterpriseService.updateEnterprise(name, desc, slogan, address, email, nif, stat, rcs,fileData);
+        phoneService.updatePhoneNumberEnterprise(phoneNumbers,enterpriseService.getEnterprise());
+        return "redirect:/index";
+    }
+}
--- src/main/java/com/example/prog3/Service/EnterpriseService.java
+++ src/main/java/com/example/prog3/Service/EnterpriseService.java
@@ -5,6 +5,7 @@ import com.example.prog3.model.Enterprise;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +15,26 @@ public class EnterpriseService {
     public Enterprise getEnterprise(){
         return repository.findAll().get(0);
     }
-    public void updateEnterprise(){
+    public void updateEnterprise(
+            String name ,
+            String desc,
+            String slogan,
+            String address,
+            String email,
+            String nif,
+            String stat,
+            String rcs,
+            byte[] logo){
+        Enterprise enterprise = getEnterprise();
+        enterprise.setName(name);
+        enterprise.setDescription(desc);
+        enterprise.setSlogan(slogan);
+        enterprise.setAddress(address);
+        enterprise.setEmail(email);
+        enterprise.setNif(nif);
+        enterprise.setStat(stat);
+        enterprise.setRcs(rcs);
+        enterprise.setLogo(logo);
+        repository.save(enterprise);
     }
 }
